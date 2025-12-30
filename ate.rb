eb5ad1ccec097580736b0678a5198c0ba2b8e408
Taylor Mohney <taylormohneytech@gmail.com>
2025-12-29 21:20:28 -0800

    fix: Update URLs to use public tap release artifacts

--- ate.rb
+++ ate.rb
@@ -5,10 +5,10 @@ class Ate < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kindlyrobotics/monorepo/releases/download/cli-v0.2.7/ate-macos-arm64"
+      url "https://github.com/kindlyrobotics/homebrew-tap/releases/download/v0.2.7/ate-macos-arm64"
       sha256 "77e46cfd31964d89f5721084d3073939ba0a7a50276b3b16804a8e3f5652bdb8"
     else
-      url "https://github.com/kindlyrobotics/monorepo/releases/download/cli-v0.2.7/ate-macos-x86_64"
+      url "https://github.com/kindlyrobotics/homebrew-tap/releases/download/v0.2.7/ate-macos-x86_64"
       sha256 "b9540363cb928d60f10b39295a72ed3512823f2f84edc30f5d0e877ae6628692"
     end
   end
